ac161579be8f155d0b1ca06cb0fd0f6eeaeaa52e
Maharoz <mahruj66@gmail.com>
2018-10-18 23:17:09 +0600

    Post selection added

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state={
-        posts:[]
+        posts:[],
+        selectedPostId:null ,
     }
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -22,12 +23,18 @@ class Blog extends Component {
 
             });
             this.setState({posts:updatedPosts});
-            //console.log(response);
+            console.log(response);
         });
+    }
+    postSelectedHandler = (id)=>{
+        this.setState({selectedPostId:id});
     }
     render () {
         const posts = this.state.posts.map(post=>{
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post key={post.id} 
+            title={post.title} 
+            author={post.author}
+            clicked={()=> this.postSelectedHandler(post.id)} />
         });
         return (
             <div>
@@ -35,7 +42,7 @@ class Blog extends Component {
                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id ={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
