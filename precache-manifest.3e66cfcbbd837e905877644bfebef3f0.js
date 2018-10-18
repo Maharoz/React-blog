e3eb0950b31fbb145923bd5e42f8a31359f66d4a
Maharoz <mahruj66@gmail.com>
2018-10-18 22:24:57 +0600

    Updates

---
+++ precache-manifest.3e66cfcbbd837e905877644bfebef3f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82de7d5569f6efc374eb",
+    "url": "/React-blog/static/js/runtime~main.82de7d55.js"
+  },
+  {
+    "revision": "221a8242446540ae3e8f",
+    "url": "/React-blog/static/js/main.221a8242.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/React-blog/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "221a8242446540ae3e8f",
+    "url": "/React-blog/static/css/main.e5d5f91c.chunk.css"
+  },
+  {
+    "revision": "2d0a0910721847ea81f5c5319206e782",
+    "url": "/React-blog/index.html"
+  }
+];
