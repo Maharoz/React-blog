a146006bd0d70a9b0a39759c1bb9f98629a653ed
Maharoz <mahruj66@gmail.com>
2018-10-20 22:04:56 +0600

    error fixed

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import axios from 'axios';
 import Axios from 'axios';
 
 axios.defaults.baseURL='https://jsonplaceholder.typicode.com';
-axios.defaults.headers.coommon['Authorization'] = 'AUTH TOKEN';
-Axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(request =>{
     console.log(request);
