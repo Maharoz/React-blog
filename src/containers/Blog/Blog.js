ebec5b0407ecb0a450a050a86823936575d307af
Maharoz <mahruj66@gmail.com>
2018-11-08 21:22:10 +0600

    routing mechanism done

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 import Posts from './Posts/Posts';
 import {Route} from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 
 
 import './Blog.css';
@@ -30,6 +31,7 @@ class Blog extends Component {
                <Route path="/" render={()=><h1>Home2</h1>} /> */}
 
                <Route path="/" exact component={Posts}/>
+               <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
